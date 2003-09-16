c2fd5d63c7d02eccaa709cdb552f0df9ae0eed0d
teerex <>
2003-09-16 17:59:24 +0000

    all macroing functions

---
+++ win32/DAOCSkilla/Macroing.dfm
@@ -0,0 +1,101 @@
+object frmMacroing: TfrmMacroing
+  Left = 253
+  Top = 238
+  Width = 498
+  Height = 104
+  Caption = 'Macroing'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  PixelsPerInch = 96
+  TextHeight = 13
+  object btnPowerskillBuy: TSpeedButton
+    Left = 4
+    Top = 48
+    Width = 117
+    Height = 25
+    Caption = 'Powerskill Buy'
+    OnClick = btnPowerskillBuyClick
+  end
+  object Label1: TLabel
+    Left = 4
+    Top = 4
+    Width = 477
+    Height = 13
+    Caption = 
+      'Macroing is active while the appropriate window is open.  Do not' +
+      ' use if you have not read the source.'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clMaroon
+    Font.Height = -11
+    Font.Name = 'MS Sans Serif'
+    Font.Style = []
+    ParentFont = False
+  end
+  object chkAutosell: TCheckBox
+    Left = 128
+    Top = 24
+    Width = 205
+    Height = 17
+    Caption = 'Autosell hinges and all powerskill items'
+    TabOrder = 0
+    OnClick = chkAutosellClick
+  end
+  object btnMacroTradeskill: TButton
+    Left = 128
+    Top = 48
+    Width = 101
+    Height = 25
+    Caption = 'Macro Tradeskill'
+    TabOrder = 1
+    OnClick = btnMacroTradeskillClick
+  end
+  object btnAFK: TButton
+    Left = 236
+    Top = 48
+    Width = 89
+    Height = 25
+    Caption = 'AFK Message'
+    TabOrder = 2
+    OnClick = btnAFKClick
+  end
+  object btnTellMacro: TButton
+    Left = 332
+    Top = 48
+    Width = 75
+    Height = 25
+    Caption = 'MacroScript'
+    TabOrder = 3
+    OnClick = btnTellMacroClick
+  end
+  object btnSpellcraftHlp: TButton
+    Left = 412
+    Top = 48
+    Width = 75
+    Height = 25
+    Caption = 'Spellcrft Help'
+    TabOrder = 4
+    OnClick = btnSpellcraftHlpClick
+  end
+  object btnShowMapModes: TButton
+    Left = 4
+    Top = 22
+    Width = 117
+    Height = 22
+    Caption = 'Show nav map nodes'
+    TabOrder = 5
+    OnClick = btnShowMapModesClick
+  end
+  object tmrTimeoutDelay: TTimer
+    Enabled = False
+    OnTimer = tmrTimeoutDelayTimer
+    Left = 344
+    Top = 20
+  end
+end
---
+++ win32/DAOCSkilla/Macroing.pas
@@ -0,0 +1,297 @@
+unit Macroing;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons, DAOCControl, PowerSkill, ExtCtrls;
+
+type
+  TfrmMacroing = class(TForm)
+    btnPowerskillBuy: TSpeedButton;
+    Label1: TLabel;
+    chkAutosell: TCheckBox;
+    btnMacroTradeskill: TButton;
+    btnAFK: TButton;
+    btnTellMacro: TButton;
+    btnSpellcraftHlp: TButton;
+    btnShowMapModes: TButton;
+    tmrTimeoutDelay: TTimer;
+    procedure btnShowMapModesClick(Sender: TObject);
+    procedure btnPowerskillBuyClick(Sender: TObject);
+    procedure chkAutosellClick(Sender: TObject);
+    procedure btnMacroTradeskillClick(Sender: TObject);
+    procedure btnAFKClick(Sender: TObject);
+    procedure btnTellMacroClick(Sender: TObject);
+    procedure btnSpellcraftHlpClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure tmrTimeoutDelayTimer(Sender: TObject);
+  private
+    FDControl: TDAOCControl;
+    FAutoSell:      boolean;
+    FInSellOff:     boolean;
+    FPSItemList:    TPowerSkillItemList;
+
+    procedure DoAutoSell;
+    procedure DoAutoBuy;
+    function CheckConflictingWindows(AWindowList: array of const) : boolean;
+    procedure CheckNeedMorePSMaterials;
+    procedure Log(const s: string);
+  public
+    procedure DAOCInventoryChanged;
+    procedure DAOCVendorWindow;
+    procedure DAOCPathChanged;
+    
+    property DAOCControl: TDAOCControl read FDControl write FDControl;
+  end;
+
+var
+  frmMacroing: TfrmMacroing;
+
+implementation
+
+uses ShowMapNodes, PowerSkillSetup, SpellcraftHelp, MacroTradeSkill,
+  AFKMessage, TellMacro, DAOCWindows, DAOCInventory, Unit1;
+
+{$R *.dfm}
+
+const
+  TIMEOUT_AUTOSELL = 1;
+  TIMEOUT_AUTOBUY = 2;
+
+procedure TfrmMacroing.btnShowMapModesClick(Sender: TObject);
+begin
+  if frmShowMapNodes.Visible then
+    frmShowMapNodes.Close
+  else
+    frmShowMapNodes.Show;
+  frmShowMapNodes.ShowMapNodes(FDControl.MapNodes);
+end;
+
+procedure TfrmMacroing.btnPowerskillBuyClick(Sender: TObject);
+begin
+  frmPowerSkill.DAOCControl := FDControl;
+  frmPowerSkill.PSItemList := FPSItemList;
+
+  if not CheckConflictingWindows([frmSpellcraftHelp]) then
+    exit;
+
+  if frmPowerSkill.Visible then
+    frmPowerSkill.Close
+  else
+    frmPowerSkill.Show;
+end;
+
+procedure TfrmMacroing.chkAutosellClick(Sender: TObject);
+begin
+  FAutoSell := chkAutoSell.Checked;
+end;
+
+procedure TfrmMacroing.btnMacroTradeskillClick(Sender: TObject);
+begin
+  frmMacroTradeSkills.DAOCControl := FDControl;
+
+//  if not CheckConflictingWindows([frmSpellcraftHelp]) then
+//    exit;
+
+  if frmMacroTradeSkills.Visible then
+    frmMacroTradeSkills.Close
+  else
+    frmMacroTradeSkills.Show;
+end;
+
+procedure TfrmMacroing.btnAFKClick(Sender: TObject);
+begin
+  frmAFK.DAOCControl := FDControl;
+  if frmAFK.Visible then
+    frmAFK.Close
+  else
+    frmAFK.Show;
+end;
+
+procedure TfrmMacroing.btnTellMacroClick(Sender: TObject);
+begin
+{$IFDEF DAOC_AUTO_SERVER}
+  frmTellMacro.DAOCControl := FDControl;
+  if frmTellMacro.Visible then
+    frmTellMacro.Close
+  else
+    frmTellMacro.Show;
+{$ENDIF DAOC_AUTO_SERVER}
+end;
+
+procedure TfrmMacroing.btnSpellcraftHlpClick(Sender: TObject);
+begin
+  frmSpellcraftHelp.DAOCControl := FDControl;
+
+  if not CheckConflictingWindows([frmPowerskill]) then
+    exit;
+
+  if frmSpellcraftHelp.Visible then
+    frmSpellcraftHelp.Close
+  else
+    frmSpellcraftHelp.Show;
+end;
+
+procedure TfrmMacroing.FormCreate(Sender: TObject);
+begin
+  FPSItemList := TPowerSkillItemList.Create;
+end;
+
+procedure TfrmMacroing.FormDestroy(Sender: TObject);
+begin
+  FreeAndNil(FPSItemList);
+end;
+
+procedure TfrmMacroing.DoAutoSell;
+var
+  I:    integer;
+  pWnd: TStatsWindow;
+  iCnt: integer;
+  pFirstItem:   TDAOCInventoryItem;
+begin
+  if FDControl.SelectedID = 0 then begin
+//    Log('InvChg: Timer popped with noone selected');
+    exit;
+  end;
+
+  iCnt := 0;
+  pFirstItem := nil;
+  with FDControl.LocalPlayer.Inventory do
+    for I := 0 to Count - 1 do
+      if Items[I].IsInBag and (Items[I].Quality <> 100) and
+        (Assigned(FPSItemList.Find(Items[I].CountlessDescription)) or
+        (Pos('cloak', Items[I].Description) > 0) or
+        (Pos('hinge', Items[I].Description) > 0)) then begin
+        inc(iCnt);
+        if not Assigned(pFirstItem) then
+          pFirstItem := Items[I];
+      end;  { If match }
+
+  if (FInSellOff or (iCnt >= Random(7) + 1)) and Assigned(pFirstItem) then begin
+    // emo1.Lines.Add('InvChg: Found item - ' + Items[I].Description);
+    pWnd := TStatsWindow.Create(FDControl.WindowManager);
+    pWnd.SelectInventoryBag(pFirstItem.BagPage);
+    sleep(200);
+    pWnd.SelectInventoryItem(pFirstItem.BagItemIndex);
+    pWnd.Free;
+    sleep(500);
+    FDControl.DoSendKeys('[f2]');
+    FInSellOff := iCnt > 0;
+    exit;
+  end;
+
+  FInSellOff := false;
+//  Log('InvChg: could not find N items');
+  CheckNeedMorePSMaterials;
+end;
+
+procedure TfrmMacroing.DoAutoBuy;
+var
+  bWasAutoSell:   boolean;
+begin
+  bWasAutoSell := FAutoSell;
+  FAutoSell := false;
+
+  if frmPowerskill.Visible then begin
+    frmPowerskill.ExecutePurchases;
+
+    if frmPowerskill.KeepBuying and
+      AnsiSameText(FDControl.NodeClosestToPlayerPos.Name, FPSItemList.MerchantNodeName) then
+      FDControl.PathToNodeName(FPSItemList.ForgeNodeName);
+  end
+
+  else if frmSpellcraftHelp.Visible then
+    frmSpellcraftHelp.ExecutePurchases;
+
+  FAutoSell := bWasAutoSell;
+  Log('Purchase complete');
+end;
+
+function TfrmMacroing.CheckConflictingWindows(AWindowList: array of const): boolean;
+var
+  I:    integer;
+  sVisWindows:  string;
+begin
+  sVisWindows := '';
+  for I := low(AWindowList) to high(AWindowList) do
+    if TForm(AWindowList[I].VObject).Visible then
+      sVisWindows := sVisWindows + #9 + TForm(AWindowList[I].VObject).Caption + #13;
+
+  if sVisWindows = '' then begin
+    Result := true;
+    exit;
+  end;
+
+  Result := MessageDlg('That function conflicts with the operation of the following windows:'#13 +
+    sVisWindows +
+    'If you continue, those windows will be closed.',
+    mtWarning, [mbOk, mbCancel], 0) = mrOK;
+
+  if Result then
+    for I := low(AWindowList) to high(AWindowList) do
+      if TForm(AWindowList[I].VObject).Visible then
+        TForm(AWindowList[I].VObject).Close;
+end;
+
+procedure TfrmMacroing.CheckNeedMorePSMaterials;
+begin
+  if frmPowerskill.Visible then begin
+    if not AnsiSameText(FDControl.NodeClosestToPlayerPos.Name, FPSItemList.ForgeNodeName) then
+      exit;
+
+    if not frmPowerskill.HasMaterialsForItem then
+      FDControl.PathToNodeName(FPSItemList.MerchantNodeName)
+  end;
+end;
+
+
+procedure TfrmMacroing.Log(const s: string);
+begin
+  frmMain.Log(s);
+end;
+
+procedure TfrmMacroing.DAOCInventoryChanged;
+begin
+  if not (Visible and FAutoSell) then
+    exit;
+
+  if FDControl.SelectedID = 0 then
+    exit;
+
+  tmrTimeoutDelay.Tag := TIMEOUT_AUTOSELL;
+  // Log('Setting timer to TIMEOUT_AUTOSELL');
+  tmrTimeoutDelay.Enabled := false;
+  tmrTimeoutDelay.Enabled := true;
+end;
+
+procedure TfrmMacroing.tmrTimeoutDelayTimer(Sender: TObject);
+begin
+  tmrTimeoutDelay.Enabled := false;
+
+  case tmrTimeoutDelay.Tag of
+    TIMEOUT_AUTOSELL:
+      DoAutoSell;
+    TIMEOUT_AUTOBUY:
+      DoAutoBuy;
+  end;
+end;
+
+procedure TfrmMacroing.DAOCVendorWindow;
+begin
+  if frmPowerskill.Visible or frmSpellcraftHelp.Visible then begin
+    Log('Setting timer to TIMEOUT_AUTOBUY');
+    tmrTimeoutDelay.Tag := TIMEOUT_AUTOBUY;
+    tmrTimeoutDelay.Enabled := false;
+    tmrTimeoutDelay.Enabled := true;
+  end;
+end;
+
+procedure TfrmMacroing.DAOCPathChanged;
+begin
+  if frmShowMapNodes.Visible then
+    frmShowMapNodes.PathChanged(FDControl.CurrentPath);
+end;
+
+end.
