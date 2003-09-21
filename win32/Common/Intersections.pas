098049423658ebff74c3712800c53be2e6368dc6
teerex <>
2003-09-21 01:11:26 +0000

    some basic intersection math

---
+++ win32/Common/Intersections.pas
@@ -0,0 +1,37 @@
+unit Intersections;
+
+interface
+
+uses
+  Types;
+  
+function PointInRect(const ARect: TRect; AX, AY: integer) : boolean;
+function RectsIntersect(const A, B: TRect) : boolean;
+
+implementation
+
+function PointInRect(const ARect: TRect; AX, AY: integer) : boolean;
+{ point in rect assumes a top down rectangle (that the Top is less than the Bottom }
+begin
+  Result := (AX >= ARect.Left) and (AX <= ARect.Right) and
+    (AY >= ARect.Top) and (AY <= ARect.Bottom);
+end;
+
+function RectsIntersect(const A, B: TRect) : boolean;
+begin
+    { a rect intersects if any one of its points is inside the other
+      dude's rect.  We need to check both against each other because
+      one might completely contain the other, in which case only
+      one of the two checks will be true }
+  Result := PointInRect(A, B.Left, B.Top) or
+    PointInRect(A, B.Right, B.Top) or
+    PointInRect(A, B.Left, B.Bottom) or
+    PointInRect(A, B.Right, B.Bottom)
+      or
+    PointInRect(B, A.Left, A.Top) or
+    PointInRect(B, A.Right, A.Top) or
+    PointInRect(B, A.Left, A.Bottom) or
+    PointInRect(B, A.Right, A.Bottom);
+end;
+
+end.
