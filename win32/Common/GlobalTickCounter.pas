52e34c608b4a94d029594df699e72685d1cac602
teerex <>
2003-10-04 13:37:25 +0000

    now supports both global timer and instant timer

--- win32/Common/GlobalTickCounter.pas
+++ win32/Common/GlobalTickCounter.pas
@@ -5,17 +5,30 @@ interface
 uses
   Windows;
   
+{$IFDEF GLOBAL_TICK_COUNTER}
 var
   GlobalTickCount:  Cardinal;
-  
 procedure UpdateGlobalTickCount;
+{$ELSE}
+function GlobalTickCount : Cardinal;
+procedure UpdateGlobalTickCount;
+{$ENDIF GLOBAL_TICK_COUNTER}
 
 implementation
 
 procedure UpdateGlobalTickCount;
 begin
+{$IFDEF GLOBAL_TICK_COUNTER}
   GlobalTickCount := GetTickCount;
+{$ENDIF GLOBAL_TICK_COUNTER}
 end;
+
+{$IFNDEF GLOBAL_TICK_COUNTER}
+function GlobalTickCount : Cardinal;
+begin
+  Result := GetTickCount;
+end;
+{$ENDIF}
 
 end.
  
