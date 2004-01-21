a52bc4746a1f28e6755195c81c5f9f00093ed912
sf_devel <>
2004-01-21 05:14:58 +0000

    reimplemented the intercept equation using proportional navigation

--- win32/Cheyenne/Utils/CodeUtils.h
+++ win32/Cheyenne/Utils/CodeUtils.h
@@ -89,7 +89,7 @@ template<typename T> struct INTERCEPT_PARAMS
     T a0x; // reference initial x position
     T a0y; // reference initial y position
     T tvx; // target x velocity
-    T tvy; // target y velicity
+    T tvy; // target y velocity
     T s; // constant speed of reference
 }; // end INTERCEPT_PARAMS
 
@@ -113,340 +113,89 @@ template<typename T> struct INTERCEPT_DATA
     than once for a given intercept because reality dictates that
     nothing accelerates or turns instantly. Stupid reality...
     
-    What it does it this (and this is straight from my notes, sorry):
-    
-    define:
-        t0 as initial time
-        At as reference position at time t
-        A0 as reference position at time t0
-        Tt as target position at time t
-        T0 as reference position at time t0
-        Av as reference velocity to intercept (this is an output of the algorithm)
-        Tv as target velocity at time (assumed constant, if it is not then
-            you will have to call this function more than once)
-        s as squareroot-of-the-sum-of-the-squares: |Av|
-    
-    define:
-        At = A0+(Av*(t-t0))
-        Tt = T0+(Tv*(t-t0))
-        
-    then:
-        For intercept at time t, At==Tt
-        Setting At and Tt to be equal and rearranging to solve for t:
-        
-        {equation 0}
-            t=((A0-T0)/Tv-Av)+t0
-        
-        This is the time-of-intercept
-    
-    therefore:
-        We must minimize the time-to-intercept (t)
-        
-    define:
-        Avx as x component of velocity for reference to intercept target
-        Avy as y component of velocity for reference to intercept target
-        Tvx as x component of velocity of target
-        Tvy as y component of velocity of target
-        
-        But Avx and Avy are related: Avy is +/- (s^2 - Avx^2)^0.5
-        
-    therefore:
-        Time of x intercept and y intercept is the same, so
-        <-- from above, rearrange: t=((A0-T0)/Tv-Av)+t0 -->
-        <-- the t cancels and so does t0 -->
-        
-        {equation 1}
-            (A0x-T0x)/(Tvx-Avx) = (A0y-T0y)/(Tvy-Avy)
-
-    define:
-        Dx as T0x-A0x
-        Dy as T0y-A0y
-        
-    therefore:
-        Rearrange equation 1:
-        {equation 2}
-            (Dx*(Tvy-Avy)/Dy) - (Tvx-Avx) = 0
-        
-        To minimize a function f() of 1 variable over a given range:
-            (Avx and Avy are both unknown, but they are related)
-            (the range is [-s,+s] -- we have a capped velocity!)
-            (we want to minimize t, which is what equation 2 is in terms of)
-            
-        1. We take the derivative of f()
-        2. Find the critical points
-        3. Solve f(c) for all critical points
-        4. Solve for f(-s) and f(+s)
-        5. The minimum of f() over the range [-s,+s] is 
-           the value computed in steps 3 and 4 that, when
-           plugged in to equation 0, gives the smallest result.
-           
-        d/dAvx for equation 2 (f'()) is:
-        {equation 3}
-            1-((Dx*Avx/Dy)/sqrt(s^2-Avx^2))
-        
-        Performing steps 2-4 on equation 3 gives 4 possible solutions:
-            (you must use the quadratic equation to solve this one)
-        
-        ************************************************************
-        +s,
-        -s,
-        +(sqrt(4*((Dx^2+Dy^2)/Dy^2)*s^2) )/2*((Dx^2+Dy^2)/Dy^2)),
-        -(sqrt(4*((Dx^2+Dy^2)/Dy^2)*s^2) )/2*((Dx^2+Dy^2)/Dy^2))
-        ************************************************************
-        
-        We have to be careful here: because of the nature of the problem, 
-        we can solve for Avx and still not know Avy because there are 2 possible
-        Avy solutions (remember that Avy = sqrt(s^2-Avx^2)). This is why 
-        Find intercept time tests each solution effectively 4 times:
-        +x,+y
-        -x,-y
-        +x,-y
-        -x,+y
-        
-        The result is the solution that gives the best
-        sensible (non-zero, non-infinite) time of intercept.
-        
-        This result is passed up to FindIntercept() and
-        the actual Avx and Avy values are determined based
-        upon knowing the time of intercept.
-        
-        SO: call FindIntercept() and you will get a reasonable result. 
-        If it returns false, then you are either at the intercept point already
-        or there is no possible way to intercept the target.
-        
-        I think :D
+    The method implemented below is known as "Proportional Navigation"
+    and is the basis for many missile intercept equations. Look it
+    up on the 'net, many people have written excellent papers, theses,
+    etc. on the subject. This is the only free C++ implementation that 
+    I know of. Feel free to make use of it!
 */
-template<typename T> T FindInterceptTime
-    (
-    const INTERCEPT_PARAMS<T>& params
-    )
+template<typename T> bool FindIntercept(INTERCEPT_DATA<T>& id)
 {
-    const T dx=params.a0x-params.t0x;
-    const T dy=params.a0y-params.t0y;
-    const T dx_squared=dx*dx;
-    const T dy_squared=dy*dy;
+    const T DeltaRx=id.params.t0x-id.params.a0x; // x range to target
+    const T DeltaRy=id.params.t0y-id.params.a0y; // y range to target
+    const T DeltaR=sqrt(DeltaRx*DeltaRx + DeltaRy*DeltaRy); // total range to target
+    const T St=sqrt(id.params.tvx*id.params.tvx + id.params.tvy*id.params.tvy); // target speed
     
-    // possible solutions:
-    // +/- s
-    // quadratic
-    // 4 total possible solutions
-    T solution[4];
-    solution[0]=params.s;
-    solution[1]=-params.s;
-    
-    if(not_near(dy_squared,T(0)) && not_near(dx_squared+dy_squared,T(0)))
+    // can't do const here because this must be computed
+    T CosTheta; // theta is angle between DeltaR vector and target speed vector
+    if(is_near(DeltaR*St,T(0)))
         {
-        solution[2]=
-            sqrt(T(4)*((dx_squared+dy_squared)/dy_squared)*params.s*params.s) /
-            (T(2)*((dx_squared+dy_squared)/dy_squared));
-
-        solution[3]=
-            -(sqrt(T(4)*((dx_squared+dy_squared)/dy_squared)*params.s*params.s)) /
-            (T(2)*((dx_squared+dy_squared)/dy_squared));
-    
+        // we have a 0/0 case here. Taking the limit and whatnot, 
+        // CosTheta is 1 (theta is 0°)
+        CosTheta=T(1);
         }
     else
         {
-        solution[2]=T(0);
-        solution[3]=T(0);
+        CosTheta=(DeltaRx*id.params.tvx + DeltaRy*id.params.tvy) / (DeltaR*St);
         }
-        
-    // find
-    T avx_temp=T(0);
-    T avy_temp=T(0);
-    T delta_t=std::numeric_limits<T>::max();
-    T delta_t_temp;
     
-    for(int i=0;i<4;++i)
-        {
-        // take y solution
-        avy_temp=solution[i];
-        avx_temp=sqrt(params.s*params.s - avy_temp*avy_temp); // @@ check both + and - values here!!!
-        
-        // check for infinity in the division
-        if(not_near(params.tvy,avy_temp))
-            {
-            // find how long until y gets to where it needs to be
-            delta_t_temp=((params.a0y-params.t0y)/(params.tvy-avy_temp));
-            
-            // find how long until x gets to where it needs to be
-            // check for infinity in the division
-            if(not_near(params.tvx,avx_temp))
-                {
-                if(not_near(delta_t_temp,((params.a0x-params.t0x)/(params.tvx-avx_temp))))
-                    {
-                    // not a valid solution, no intercept!
-                    delta_t_temp=std::numeric_limits<T>::max();
-                    }
-                }
-            else if(not_near(params.a0x,params.t0x))
-                {
-                // not a valid solution, no intercept!
-                delta_t_temp=std::numeric_limits<T>::max();
-                }
-            }
-        else
-            {
-            // not a valid solution, no intercept!
-            delta_t_temp=std::numeric_limits<T>::max();
-            }
-        
-        if(delta_t_temp < delta_t && not_near(delta_t_temp,T(0)) && delta_t_temp>T(0))
-            {
-            delta_t=delta_t_temp;
-            continue;
-            }
-        
-        // check the x solution
-        avy_temp=avx_temp;
-        avx_temp=solution[i];
-        
-        // check for infinity in the division
-        if(not_near(params.tvx,avx_temp))
-            {
-            // find how long until x gets to where it needs to be
-            delta_t_temp=((params.a0x-params.t0x)/(params.tvx-avx_temp));
-
-            // find how long until y gets to where it needs to be
-            // check for infinity in the division
-            if(not_near(params.tvy,avy_temp))
-                {
-                if(not_near(delta_t_temp,((params.a0y-params.t0y)/(params.tvy-avy_temp))))
-                    {
-                    // not a valid solution, no intercept!
-                    delta_t_temp=std::numeric_limits<T>::max();
-                    }
-                }
-            else if(not_near(params.a0y,params.t0y))
-                {
-                // not a valid solution, no intercept!
-                delta_t_temp=std::numeric_limits<T>::max();
-                }
-            }
-        else
-            {
-            // not a valid solution, no intercept!
-            delta_t_temp=std::numeric_limits<T>::max();
-            }
-            
-        if(delta_t_temp < delta_t && not_near(delta_t_temp,T(0)) && delta_t_temp>T(0))
-            {
-            delta_t=delta_t_temp;
-            continue;
-            }
-
-        // take y solution with avx=-avx (sqrt only gives + answers)
-        avy_temp=solution[i];
-        avx_temp=-sqrt(params.s*params.s - avy_temp*avy_temp); // @@ check both + and - values here!!!
-        
-        // check for infinity in the division
-        if(not_near(params.tvy,avy_temp))
-            {
-            // find how long until y gets to where it needs to be
-            delta_t_temp=((params.a0y-params.t0y)/(params.tvy-avy_temp));
-            
-            // find how long until x gets to where it needs to be
-            // check for infinity in the division
-            if(not_near(params.tvx,avx_temp))
-                {
-                if(not_near(delta_t_temp,((params.a0x-params.t0x)/(params.tvx-avx_temp))))
-                    {
-                    // not a valid solution, no intercept!
-                    delta_t_temp=std::numeric_limits<T>::max();
-                    }
-                }
-            else if(not_near(params.a0x,params.t0x))
-                {
-                // not a valid solution, no intercept!
-                delta_t_temp=std::numeric_limits<T>::max();
-                }
-            }
-        else
-            {
-            // not a valid solution, no intercept!
-            delta_t_temp=std::numeric_limits<T>::max();
-            }
-        
-        if(delta_t_temp < delta_t && not_near(delta_t_temp,T(0)) && delta_t_temp>T(0))
-            {
-            delta_t=delta_t_temp;
-            continue;
-            }
-        
-        // check the x solution
-        avy_temp=avx_temp;
-        avx_temp=solution[i];
-        
-        // check for infinity in the division
-        if(not_near(params.tvx,avx_temp))
-            {
-            // find how long until x gets to where it needs to be
-            delta_t_temp=((params.a0x-params.t0x)/(params.tvx-avx_temp));
-
-            // find how long until y gets to where it needs to be
-            // check for infinity in the division
-            if(not_near(params.tvy,avy_temp))
-                {
-                if(not_near(delta_t_temp,((params.a0y-params.t0y)/(params.tvy-avy_temp))))
-                    {
-                    // not a valid solution, no intercept!
-                    delta_t_temp=std::numeric_limits<T>::max();
-                    }
-                }
-            else if(not_near(params.a0y,params.t0y))
-                {
-                // not a valid solution, no intercept!
-                delta_t_temp=std::numeric_limits<T>::max();
-                }
-            }
-        else
-            {
-            // not a valid solution, no intercept!
-            delta_t_temp=std::numeric_limits<T>::max();
-            }
-            
-        if(delta_t_temp < delta_t && not_near(delta_t_temp,T(0)) && delta_t_temp>T(0))
-            {
-            delta_t=delta_t_temp;
-            continue;
-            }
-        } // end for all possible solutions
-
-    // done
-    return(delta_t);
-} // end FindInterceptTime
-
-/*
-    For a description of this, see comment for FindInterceptTime
-*/
-template<typename T> bool FindIntercept(INTERCEPT_DATA<T>& intercept_data)
-{
-    intercept_data.TimeToIntercept=FindInterceptTime(intercept_data.params);
+    // use trigonometric identity to get this one
+    // we ignore the +- thing...
+    const T SinTheta=sqrt(T(1) - (CosTheta*CosTheta)); 
     
-    if(intercept_data.TimeToIntercept < std::numeric_limits<T>::max() && not_near(intercept_data.TimeToIntercept,T(0)))
+    // alpha is the DeltaR vector and a vector from the reference ("a" in INTERCEPT_DATA::INTERCEPT_PARAMS)
+    // to the predicted intercept point
+    // s (speed of reference "a") can NOT be zero!
+    const T SinAlpha=SinTheta*(St/id.params.s);
+    
+    // check for a solution: if |SinAlpha| > 1 then no intercept is possible!
+    if(fabs(SinAlpha) > T(1))
         {
-        // we have a viable solution, get x and y intercept
-        // points then convert to speed
-        intercept_data.XIntercept=intercept_data.params.t0x+(intercept_data.TimeToIntercept*intercept_data.params.tvx);
-        intercept_data.YIntercept=intercept_data.params.t0y+(intercept_data.TimeToIntercept*intercept_data.params.tvy);
-        
-        intercept_data.XDotToIntercept=(intercept_data.XIntercept - intercept_data.params.a0x)/intercept_data.TimeToIntercept;
-        intercept_data.YDotToIntercept=(intercept_data.YIntercept - intercept_data.params.a0y)/intercept_data.TimeToIntercept;
-
-        // get angle
-        intercept_data.HeadingRadiansToIntercept=atan2(intercept_data.XDotToIntercept,intercept_data.YDotToIntercept);
-        
-        // correct to positive as necessary
-        if(intercept_data.HeadingRadiansToIntercept < T(0))
-            {
-            intercept_data.HeadingRadiansToIntercept += T(2)*T(3.1415926535897932384626433832795);
-            }
-        
-        return(true);
-        } // end if FindInterceptTime returned a workable time
-    else
-        {
+        // no possible intercept!
         return(false);
-        } // end else FindInterceptTime returned an unworkable time
-} // end FindIntercept
+        }
+    
+    // use trigonometric identity to get this one
+    // again, we ignore the +- thing...
+    const T CosAlpha=sqrt(T(1)-(SinAlpha*SinAlpha));
+    
+    // compute ideal time-to-go (ttg). Can't use const
+    // because this must be computed
+    T ttg;
+    {
+    T temp=(id.params.s*CosAlpha) - (St*CosTheta);
+    
+    if(temp > T(0))
+        {
+        ttg=DeltaR/temp;
+        }
+    else
+        {
+        ttg=0; // intercept already occured (!)
+        id.TimeToIntercept=ttg;
+        id.XIntercept=id.params.t0x;
+        id.YIntercept=id.params.t0y;
+        id.XDotToIntercept=T(0);
+        id.YDotToIntercept=T(0);
+        id.HeadingRadiansToIntercept=T(0);
+        return(true);
+        }
+    }
 
+    // populate id
+    id.TimeToIntercept=ttg; // time to go
+    id.XIntercept=id.params.t0x + (ttg*id.params.tvx); // simple position integration
+    id.YIntercept=id.params.t0y + (ttg*id.params.tvy); // simple position integration
+    id.XDotToIntercept=(id.XIntercept-id.params.a0x) / ttg; // simple position integration
+    id.YDotToIntercept=(id.YIntercept-id.params.a0y) / ttg; // simple position integration
+    id.HeadingRadiansToIntercept=atan2(id.XDotToIntercept,id.YDotToIntercept);
+    
+    // give >0 headings only
+    if(id.HeadingRadiansToIntercept < T(0))
+        {
+        id.HeadingRadiansToIntercept+=T(2)*T(3.1415926535897932384626433832795);
+        }
+
+    // done, success!
+    return(true);
+} // end FindIntercept
