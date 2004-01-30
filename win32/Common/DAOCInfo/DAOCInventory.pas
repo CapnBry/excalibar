1d0504b6b64592ae6e46bbd506e4cc3fa5759535
teerex <>
2004-01-30 22:45:08 +0000

    Added inventoryitemlookup list, ItemID is now a word, not 2 bytes

--- win32/Common/DAOCInfo/DAOCInventory.pas
+++ win32/Common/DAOCInfo/DAOCInventory.pas
@@ -13,7 +13,7 @@ unit DAOCInventory;
 interface
 
 uses
-  SysUtils, Classes, Contnrs, DAOCClasses;
+  SysUtils, Classes, Contnrs, DAOCClasses, LinedFileStream, CSVLineParser;
 
 type
   TDAOCInventoryItem = class(TObject)
@@ -28,8 +28,7 @@ type
     FDescription: string;
     FColor: BYTE;
     FLevel: BYTE;
-    FItemIDMajor: BYTE;
-    FItemIDMinor: BYTE;
+    FItemID: WORD;
     FDelveInfo: TStrings;
 
     procedure SetDescription(const Value: string);
@@ -43,7 +42,7 @@ type
   public
     constructor Create;
     destructor Destroy; override;
-    function AsText : string;
+    function AsString(AVerbose: boolean) : string;
     function SummaryLine : string;
 
     function ClassRestriction : TDAOCCharacterClass;
@@ -59,8 +58,7 @@ type
     property Quality: integer read FQuality write FQuality;
     property Bonus: integer read FBonus write FBonus;
     property Color: BYTE read FColor write FColor;
-    property ItemIDMajor: BYTE read FItemIDMajor write FItemIDMajor;
-    property ItemIDMinor: BYTE read FItemIDMinor write FItemIDMinor;
+    property ItemID: WORD read FItemID write FItemID;
     property Level: BYTE read FLevel write FLevel;
     property IsInBag: boolean read GetIsInBag;
     property BagPage: integer read SlotAsBag;
@@ -82,11 +80,24 @@ type
     procedure ClearSlot(ASlotID: BYTE);
     function ItemInSlot(ASlotID: BYTE) : TDAOCInventoryItem;
     function IndexOfSlot(ASlotID: BYTE) : integer;
-    function AsText : string;
+    function AsString(AVerbose: boolean) : string;
     procedure TakeItem(AItem: TDAOCInventoryItem);
     function IsFull: boolean;
 
     property Items[AIndex: integer]: TDAOCInventoryItem read GetItems; default;
+  end;
+
+  TDAOCInventoryItemLookupList = class(TObject)
+  private
+    FList:  TStringList;
+    function IndexOfID(AID: Cardinal) : integer;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    procedure Clear;
+    function ItemName(AID: Cardinal) : string;
+    procedure LoadFromStream(AStrm: TStream);
   end;
 
 implementation
@@ -99,182 +110,166 @@ const
   INV_VAULTPOS_FIRST = $6E;
   INV_VAULTPOS_LAST = $95;
 
-function TDAOCInventoryItem.AsText: string;
+function TDAOCInventoryItem.AsString(AVerbose: boolean) : string;
 var
   I:    integer;
 begin
-  Result := Format('  %s (%s) quantity %d'#13#10 +
-    '    Con %d%% Dur %d%% Qual %d%% Bon %d%%'#13#10 +
-    '    Level %d Color 0x%2.2x ItemID 0x%2.2x %2.2x',
-    [FDescription, GetSlotName, FCount, FCondition, FDurability, FQuality,
-    FBonus, FLevel, FColor, FItemIDMajor, FItemIDMinor]);
+  if AVerbose then begin
+    Result := Format('  %s (%s) quantity %d'#13#10 +
+      '    Con %d%% Dur %d%% Qual %d%% Bon %d%%'#13#10 +
+      '    Level %d Color 0x%2.2x ItemID 0x%4.4x',
+      [FDescription, GetSlotName, FCount, FCondition, FDurability, FQuality,
+      FBonus, FLevel, FColor, FItemID]);
 
-  if Assigned(FDelveInfo) then
-    for I := 0 to FDelveInfo.Count - 1 do
-      Result := Result + #13#10'    ' + FDelveInfo[I];
+    if Assigned(FDelveInfo) then
+      for I := 0 to FDelveInfo.Count - 1 do
+        Result := Result + #13#10'    ' + FDelveInfo[I];
+  end
+  else
+    Result := GetSlotName + ': ' + FDescription;
 end;
 
 function TDAOCInventoryItem.ClassRestriction: TDAOCCharacterClass;
 begin
     { DO NOT use Helms to identify a class, because most classes
     use the same helm mesh }
-  case FItemIDMajor of
-    $00:  Result := ccUnknown;
-    $01:
-//      case FItemIDMinor of
-          // $50:  Result := ccRunemaster;  Helm
-          // $51:  Result := ccShadowblade; Helm
-          // $b6:  Result := ccHero; Helm
-//        else
-          Result := ccUnknown;
-//      end  { case list 0x01 }
-
-    $02:
-      case FItemIDMinor of
-        $aa:  Result := ccCabalist;  // chest
-        $b0:  Result := ccArmsman;  // chest
-        $b1:  Result := ccArmsman;  // leggings
-        $b2:  Result := ccArmsman;  // sleeves
-        $b3:  Result := ccArmsman;  // gloves
-        $b4:  Result := ccArmsman;  // boots
-        $b5:  Result := ccPaladin;  // chest
-        $b6:  Result := ccPaladin;  // leggings
-        $b7:  Result := ccPaladin;  // sleeves
-        $b8:  Result := ccPaladin;  // gloves
-        $b9:  Result := ccPaladin;  // boots
-        $ba:  Result := ccHealer;  // chest
-        $bb:  Result := ccHealer;  // leggings
-        $bc:  Result := ccHealer;  // sleeves
-        $bd:  Result := ccHealer;  // gloves
-        $be:  Result := ccHealer;  // boots
-        $bf:  Result := ccRunemaster;  // chest
-        $c0:  Result := ccRunemaster;  // leggings
-        $c1:  Result := ccRunemaster;  // sleeves
-        $c2:  Result := ccRunemaster;  // boots
-        $c3:  Result := ccRunemaster;  // gloves
-        $c4:  Result := ccHero;  // chest
-        $c5:  Result := ccHero;  // leggings
-        $c6:  Result := ccHero;  // sleeves
-        $c7:  Result := ccHero;  // gloves
-        $c8:  Result := ccHero;  // boots
-        $c9:  Result := ccCleric;  // chest
-        $ca:  Result := ccCleric;  // leggings
-        $cb:  Result := ccCleric;  // sleeves
-        $cc:  Result := ccCleric;  // gloves
-        $cd:  Result := ccCleric;  // boots
-        $ce:  Result := ccMercenary;  // chest
-        $cf:  Result := ccMercenary;  // leggings
-        $d0:  Result := ccMercenary;  // sleeves
-        $d1:  Result := ccMercenary;  // gloves
-        $d2:  Result := ccMercenary;  // boots
-        $d3:  Result := ccMinstrel;  // chest
-        $d4:  Result := ccMinstrel;  // leggings
-        $d5:  Result := ccMinstrel;  // sleeves
-        $d6:  Result := ccMinstrel;  // gloves
-        $d7:  Result := ccMinstrel;  // boots
-        $d8:  Result := ccScout;  // chest
-        $d9:  Result := ccScout;  // legs
-        $da:  Result := ccScout;  // sleeves
-        $db:  Result := ccScout;  // gloves
-        $dc:  Result := ccScout;  // boots
-        $dd:  Result := ccTheurgist;  // chest
-        $de:  Result := ccBard;  // chest
-        $df:  Result := ccBard;  // leggings
-        $e0:  Result := ccBard;  // sleeves
-        $e1:  Result := ccBard;  // gloves
-        $e2:  Result := ccBard;  // boots
-        $e3:  Result := ccDruid;  // chest
-        $e4:  Result := ccDruid;  // leggings
-        $e5:  Result := ccDruid;  // sleeves
-        $e6:  Result := ccDruid;  // gloves
-        $e7:  Result := ccDruid;  // boots
-        $e8:  Result := ccEldritch;  // chest
-        $e9:  Result := ccMentalist;  // chest
-        $ea:  Result := ccNightshade;  // chest
-        $eb:  Result := ccNightshade;  // leggings
-        $ec:  Result := ccNightshade;  // sleeves
-        $ed:  Result := ccNightshade;  // gloves
-        $ee:  Result := ccNightshade;  // boots
-        $ef:  Result := ccBerserker;  // chest
-        $f0:  Result := ccBerserker;  // leggings
-        $f1:  Result := ccBerserker;  // sleeves
-        $f2:  Result := ccBerserker;  // gloves
-        $f3:  Result := ccBerserker;  // boots
-        $f4:  Result := ccHunter;  // chest
-        $f5:  Result := ccHunter;  // leggings
-        $f6:  Result := ccHunter;  // sleeves
-        $f7:  Result := ccHunter;  // gloves
-        $f8:  Result := ccHunter;  // boots
-        $f9:  Result := ccShadowblade;  // chest
-        $fa:  Result := ccShadowblade;  // leggings
-        $fb:  Result := ccShadowblade;  // sleeves
-        $fc:  Result := ccShadowblade;  // gloves
-        $fd:  Result := ccShadowblade;  // boots
-        $fe:  Result := ccShaman;  // chest
-        $ff:  Result := ccShaman;  // leggings
-        else
-          Result := ccUnknown;
-      end;  { case list 0x02 }
-    $03:
-      case FItemIDMinor of
-        $00:  Result := ccShaman;  // sleeves
-        $01:  Result := ccShaman;  // gloves
-        $02:  Result := ccShaman;  // boots
-        $03:  Result := ccSkald;  // chest
-        $04:  Result := ccSkald;  // leggings
-        $05:  Result := ccSkald;  // sleeves
-        $06:  Result := ccSkald;  // gloves
-        $07:  Result := ccSkald;  // boots
-        $08:  Result := ccWarrior;  // chest
-        $09:  Result := ccWarrior;  // leggings
-        $0a:  Result := ccWarrior;  // sleeves
-        $0b:  Result := ccWarrior;  // gloves
-        $0c:  Result := ccWarrior;  // boots
-        $0d:  Result := ccEnchanter; // chest
-        $0e:  Result := ccBlademaster; // chest
-        $0f:  Result := ccBlademaster; // leggings
-        $10:  Result := ccBlademaster; // sleeves
-        $11:  Result := ccBlademaster; // gloves
-        $12:  Result := ccBlademaster; // boots
-        $13:  Result := ccThane;  // chest
-        $14:  Result := ccThane;  // leggings
-        $15:  Result := ccThane;  // sleeves
-        $16:  Result := ccThane;  // gloves
-        $17:  Result := ccThane;  // boots
-        $18:  Result := ccInfiltrator; // chest
-        $19:  Result := ccInfiltrator; // leggings
-        $1a:  Result := ccInfiltrator; // sleeves
-        $1b:  Result := ccInfiltrator; // gloves
-        $1c:  Result := ccInfiltrator; // boots
-        $1d:  Result := ccFriar; // chest
-        $1e:  Result := ccWizard; // chest
-        $1f:  Result := ccSpiritmaster; // chest
-        $20:  Result := ccSpiritmaster; // leggings
-        $21:  Result := ccSpiritmaster; // sleeves
-        $22:  Result := ccSpiritmaster; // gloves
-        $23:  Result := ccSpiritmaster; // boots
-        $24:  Result := ccSorcerer; // chest
-        $25:  Result := ccWarden; // chest
-        $26:  Result := ccWarden; // leggings
-        $27:  Result := ccWarden; // sleeves
-        $28:  Result := ccWarden; // gloves
-        $29:  Result := ccWarden; // boots
-        $2a:  Result := ccChampion; // chest
-        $2b:  Result := ccChampion; // leggings
-        $2c:  Result := ccChampion; // sleeves
-        $2d:  Result := ccChampion; // gloves
-        $2e:  Result := ccChampion; // boots
-        $2f:  Result := ccRanger; // chest
-        $30:  Result := ccRanger; // leggings
-        $31:  Result := ccRanger; // sleeves
-        $32:  Result := ccRanger; // gloves
-        $33:  Result := ccRanger; // boots
-        else
-          Result := ccUnknown;
-      end;  { case list 0x03 }
+  case FItemID of
+    $02aa:  Result := ccCabalist;  // chest
+    $02b0:  Result := ccArmsman;  // chest
+    $02b1:  Result := ccArmsman;  // leggings
+    $02b2:  Result := ccArmsman;  // sleeves
+    $02b3:  Result := ccArmsman;  // gloves
+    $02b4:  Result := ccArmsman;  // boots
+    $02b5:  Result := ccPaladin;  // chest
+    $02b6:  Result := ccPaladin;  // leggings
+    $02b7:  Result := ccPaladin;  // sleeves
+    $02b8:  Result := ccPaladin;  // gloves
+    $02b9:  Result := ccPaladin;  // boots
+    $02ba:  Result := ccHealer;  // chest
+    $02bb:  Result := ccHealer;  // leggings
+    $02bc:  Result := ccHealer;  // sleeves
+    $02bd:  Result := ccHealer;  // gloves
+    $02be:  Result := ccHealer;  // boots
+    $02bf:  Result := ccRunemaster;  // chest
+    $02c0:  Result := ccRunemaster;  // leggings
+    $02c1:  Result := ccRunemaster;  // sleeves
+    $02c2:  Result := ccRunemaster;  // boots
+    $02c3:  Result := ccRunemaster;  // gloves
+    $02c4:  Result := ccHero;  // chest
+    $02c5:  Result := ccHero;  // leggings
+    $02c6:  Result := ccHero;  // sleeves
+    $02c7:  Result := ccHero;  // gloves
+    $02c8:  Result := ccHero;  // boots
+    $02c9:  Result := ccCleric;  // chest
+    $02ca:  Result := ccCleric;  // leggings
+    $02cb:  Result := ccCleric;  // sleeves
+    $02cc:  Result := ccCleric;  // gloves
+    $02cd:  Result := ccCleric;  // boots
+    $02ce:  Result := ccMercenary;  // chest
+    $02cf:  Result := ccMercenary;  // leggings
+    $02d0:  Result := ccMercenary;  // sleeves
+    $02d1:  Result := ccMercenary;  // gloves
+    $02d2:  Result := ccMercenary;  // boots
+    $02d3:  Result := ccMinstrel;  // chest
+    $02d4:  Result := ccMinstrel;  // leggings
+    $02d5:  Result := ccMinstrel;  // sleeves
+    $02d6:  Result := ccMinstrel;  // gloves
+    $02d7:  Result := ccMinstrel;  // boots
+    $02d8:  Result := ccScout;  // chest
+    $02d9:  Result := ccScout;  // legs
+    $02da:  Result := ccScout;  // sleeves
+    $02db:  Result := ccScout;  // gloves
+    $02dc:  Result := ccScout;  // boots
+    $02dd:  Result := ccTheurgist;  // chest
+    $02de:  Result := ccBard;  // chest
+    $02df:  Result := ccBard;  // leggings
+    $02e0:  Result := ccBard;  // sleeves
+    $02e1:  Result := ccBard;  // gloves
+    $02e2:  Result := ccBard;  // boots
+    $02e3:  Result := ccDruid;  // chest
+    $02e4:  Result := ccDruid;  // leggings
+    $02e5:  Result := ccDruid;  // sleeves
+    $02e6:  Result := ccDruid;  // gloves
+    $02e7:  Result := ccDruid;  // boots
+    $02e8:  Result := ccEldritch;  // chest
+    $02e9:  Result := ccMentalist;  // chest
+    $02ea:  Result := ccNightshade;  // chest
+    $02eb:  Result := ccNightshade;  // leggings
+    $02ec:  Result := ccNightshade;  // sleeves
+    $02ed:  Result := ccNightshade;  // gloves
+    $02ee:  Result := ccNightshade;  // boots
+    $02ef:  Result := ccBerserker;  // chest
+    $02f0:  Result := ccBerserker;  // leggings
+    $02f1:  Result := ccBerserker;  // sleeves
+    $02f2:  Result := ccBerserker;  // gloves
+    $02f3:  Result := ccBerserker;  // boots
+    $02f4:  Result := ccHunter;  // chest
+    $02f5:  Result := ccHunter;  // leggings
+    $02f6:  Result := ccHunter;  // sleeves
+    $02f7:  Result := ccHunter;  // gloves
+    $02f8:  Result := ccHunter;  // boots
+    $02f9:  Result := ccShadowblade;  // chest
+    $02fa:  Result := ccShadowblade;  // leggings
+    $02fb:  Result := ccShadowblade;  // sleeves
+    $02fc:  Result := ccShadowblade;  // gloves
+    $02fd:  Result := ccShadowblade;  // boots
+    $02fe:  Result := ccShaman;  // chest
+    $02ff:  Result := ccShaman;  // leggings
+    $0300:  Result := ccShaman;  // sleeves
+    $0301:  Result := ccShaman;  // gloves
+    $0302:  Result := ccShaman;  // boots
+    $0303:  Result := ccSkald;  // chest
+    $0304:  Result := ccSkald;  // leggings
+    $0305:  Result := ccSkald;  // sleeves
+    $0306:  Result := ccSkald;  // gloves
+    $0307:  Result := ccSkald;  // boots
+    $0308:  Result := ccWarrior;  // chest
+    $0309:  Result := ccWarrior;  // leggings
+    $030a:  Result := ccWarrior;  // sleeves
+    $030b:  Result := ccWarrior;  // gloves
+    $030c:  Result := ccWarrior;  // boots
+    $030d:  Result := ccEnchanter; // chest
+    $030e:  Result := ccBlademaster; // chest
+    $030f:  Result := ccBlademaster; // leggings
+    $0310:  Result := ccBlademaster; // sleeves
+    $0311:  Result := ccBlademaster; // gloves
+    $0312:  Result := ccBlademaster; // boots
+    $0313:  Result := ccThane;  // chest
+    $0314:  Result := ccThane;  // leggings
+    $0315:  Result := ccThane;  // sleeves
+    $0316:  Result := ccThane;  // gloves
+    $0317:  Result := ccThane;  // boots
+    $0318:  Result := ccInfiltrator; // chest
+    $0319:  Result := ccInfiltrator; // leggings
+    $031a:  Result := ccInfiltrator; // sleeves
+    $031b:  Result := ccInfiltrator; // gloves
+    $031c:  Result := ccInfiltrator; // boots
+    $031d:  Result := ccFriar; // chest
+    $031e:  Result := ccWizard; // chest
+    $031f:  Result := ccSpiritmaster; // chest
+    $0320:  Result := ccSpiritmaster; // leggings
+    $0321:  Result := ccSpiritmaster; // sleeves
+    $0322:  Result := ccSpiritmaster; // gloves
+    $0323:  Result := ccSpiritmaster; // boots
+    $0324:  Result := ccSorcerer; // chest
+    $0325:  Result := ccWarden; // chest
+    $0326:  Result := ccWarden; // leggings
+    $0327:  Result := ccWarden; // sleeves
+    $0328:  Result := ccWarden; // gloves
+    $0329:  Result := ccWarden; // boots
+    $032a:  Result := ccChampion; // chest
+    $032b:  Result := ccChampion; // leggings
+    $032c:  Result := ccChampion; // sleeves
+    $032d:  Result := ccChampion; // gloves
+    $032e:  Result := ccChampion; // boots
+    $032f:  Result := ccRanger; // chest
+    $0330:  Result := ccRanger; // leggings
+    $0331:  Result := ccRanger; // sleeves
+    $0332:  Result := ccRanger; // gloves
+    $0333:  Result := ccRanger; // boots
     else
       Result := ccUnknown;
-  end;  { case itemmajor }
+  end;  { case itemid }
 end;
 
 constructor TDAOCInventoryItem.Create;
@@ -375,26 +370,26 @@ end;
 function TDAOCInventoryItem.SummaryLine: string;
 { Like AsText, but all on one line.  Format is:
   Name, Slot, Count, Condition, Durability, Quality, Bonus, Level, Color,
-  ItemIDMajor, ItemIDMinor, DelveInfoCount, DelveInfo0, DelveInfo1, ... }
+  ItemID, DelveInfoCount, DelveInfo0, DelveInfo1, ... }
 var
   I:    integer;
 begin
-  Result :=Format('"%s",0x%2.2x,%d,%d,%d,%d,%d,%d,%d,0x%2.2x%2.2x,%d',
+  Result :=Format('"%s",0x%2.2x,%d,%d,%d,%d,%d,%d,%d,0x%4.4x,%d',
     [CountlessDescription, Slot, Count, Condition, Durability, Quality,
-     Bonus, Level, Color, ItemIDMajor, ItemIDMinor, FDelveInfo.Count]);
+     Bonus, Level, Color, ItemID, FDelveInfo.Count]);
   for I := 0 to FDelveInfo.Count - 1 do
     Result := Result + ',"' + FDelveInfo[I] + '"';
 end;
 
 { TDAOCInventory }
 
-function TDAOCInventory.AsText: string;
+function TDAOCInventory.AsString(AVerbose: boolean) : string;
 var
   I:    integer;
 begin
   Result := '';
   for I := 0 to Count - 1 do begin
-    Result := Result + Items[I].AsText;
+    Result := Result + Items[I].AsString(AVerbose);
     if I < (Count - 1) then
       Result := Result + #13#10;
   end;
@@ -482,7 +477,7 @@ procedure TDAOCInventory.TakeItem(AItem: TDAOCInventoryItem);
 begin
   ClearSlot(AItem.Slot);
     { if the description is blank, we're just clearing the slot }
-  if (AItem.ItemIDMajor = 0) and (AItem.ItemIDMinor = 0) then  // if AItem.Description = '' then
+  if AItem.ItemID = 0 then  // if AItem.Description = '' then
     AItem.Free
   else
     Add(AItem);
@@ -498,6 +493,86 @@ begin
     if AnsiSameText(AItem, Items[I].CountlessDescription) and
       (AIncludeVault or Items[I].IsInBag) then
       inc(Result, Items[I].Count);
+end;
+
+{ TDAOCInventoryItemLookupList }
+
+procedure TDAOCInventoryItemLookupList.Clear;
+begin
+  FList.Clear;
+end;
+
+constructor TDAOCInventoryItemLookupList.Create;
+begin
+  inherited;
+  FList := TStringList.Create;
+end;
+
+destructor TDAOCInventoryItemLookupList.Destroy;
+begin
+  FList.Free;
+  inherited;
+end;
+
+function TDAOCInventoryItemLookupList.IndexOfID(AID: Cardinal): integer;
+{ quick bin search.  List should be sorted by IID }
+var
+  L, H: Integer;
+  CID:  Cardinal;
+begin
+  L := 0;
+  H := FList.Count - 1;
+
+  while L <= H do begin
+    Result := (L + H) shr 1;
+    CID := Cardinal(FList.Objects[Result]);
+    if CID = AID then
+      exit
+    else if CID < AID then
+      L := Result + 1
+    else
+      H := Result - 1;
+  end;  { while L <= H }
+
+  Result := -1;
+end;
+
+function TDAOCInventoryItemLookupList.ItemName(AID: Cardinal): string;
+var
+  I:    integer;
+begin
+  I := IndexOfID(AID);
+  if I <> -1 then
+    Result := FList[I]
+  else
+    Result := '';
+end;
+
+procedure TDAOCInventoryItemLookupList.LoadFromStream(AStrm: TStream);
+var
+  CSV:  TCSVLineParser;
+  FS:   TLinedStreamWrapper;
+begin
+  Clear;
+  FS := TLinedStreamWrapper.Create(AStrm);
+  CSV := TCSVLineParser.Create;
+  try
+      { first two lines are header }
+    FS.ReadLn;
+    FS.ReadLn;
+
+    while not FS.EOF do begin
+      CSV.DataString := FS.ReadLn;
+
+      if CSV.FieldCount < 2 then
+        continue;
+
+      FList.AddObject(CSV[1], TObject(CSV.FieldAsInt(0, 0)));
+    end;  { while !EOF }
+  finally
+    FS.Free;
+    CSV.Free;
+  end;
 end;
 
 end.
