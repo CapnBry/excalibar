745d9da4907c1c168d58c6b7e562d6a478147e57
teerex <>
2004-03-26 16:33:58 +0000

    base class for multi connection

---
+++ win32/Common/PacketSniff/DAOCConnectionList.pas
@@ -0,0 +1,200 @@
+unit DAOCConnectionList;
+
+(****************************************************************************
+**
+** Copyright (C) 2004 Bryan Mayland.  All rights reserved.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation.
+**
+****************************************************************************)
+
+interface
+
+uses
+  SysUtils, Classes, GameNetPackets, DAOCObjs, DAOCPlayerAttributes,
+  DAOCConnection, DAOCInventory, ChatParse;
+
+type
+  TDAOCConnectionNotify = procedure(Sender: TObject; AConn: TDAOCConnection) of object;
+
+  TDAOCConnectionList = class(TObject)
+  private
+    FOnDeleteConnection: TDAOCConnectionNotify;
+    FOnNewConnection: TDAOCConnectionNotify;
+    function GetCount: integer;
+    function GetItems(I: integer): TDAOCConnection;
+  protected
+    FList:    TList;
+    function NewDAOCConnectionNeeded : TDAOCConnection; virtual;
+    procedure DoSetupConnectionEventsAndProperties(AConn: TDAOCConnection); virtual;
+    procedure DoOnDeleteConnection(AConn: TDAOCConnection); virtual;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    procedure Clear;
+    function IndexOf(AConn: TDAOCConnection) : integer;
+    function IndexOfConnection(ASource: TObject; AConnectionID: Cardinal) : integer;
+    function FindConnection(ASource: TObject; AConnectionID: Cardinal) : TDAOCConnection;
+
+    function NewDAOCConnection(ASource: TObject; AConnectionID: Cardinal;
+      AServerIP: Cardinal; AClientIP: Cardinal) : TDAOCConnection;
+    procedure CloseDAOCConnection(ASource: TObject; AConnectionID: Cardinal);
+    procedure ProcessDAOCPacket(ASource: TObject; APacket: TGameNetPacket);
+
+      { properties }
+    property Count: integer read GetCount;
+    property Items[I: integer]: TDAOCConnection read GetItems; default;
+      { events }
+    property OnNewConnection: TDAOCConnectionNotify read FOnNewConnection write FOnNewConnection;
+    property OnDeleteConnection: TDAOCConnectionNotify read FOnDeleteConnection write FOnDeleteConnection; 
+  end;
+
+implementation
+
+type
+  TReintroduceDAOCConnection = class(TDAOCConnection);
+
+{ TDAOCConnectionList }
+
+procedure TDAOCConnectionList.Clear;
+var
+  I:    integer;
+begin
+  for I := 0 to Count - 1 do begin
+    with TReintroduceDAOCConnection(Items[I]) do begin
+      DoOnDisconnect;
+      Free;
+    end;
+    DoOnDeleteConnection(Items[I]);
+  end;
+
+  FList.Clear;
+end;
+
+procedure TDAOCConnectionList.CloseDAOCConnection(ASource: TObject;
+  AConnectionID: Cardinal);
+var
+  iIdx:   integer;
+begin
+  iIdx := IndexOfConnection(ASource, AConnectionID);
+  if iIdx <> -1 then begin
+    with TReintroduceDAOCConnection(Items[iIdx]) do begin
+      DoOnDisconnect;
+      Free;
+    end;
+
+    DoOnDeleteConnection(Items[iIdx]);
+
+    FList.Delete(iIdx);
+  end;
+end;
+
+constructor TDAOCConnectionList.Create;
+begin
+  FList := TList.Create;
+end;
+
+destructor TDAOCConnectionList.Destroy;
+begin
+  Clear;
+  FList.Free;
+
+  inherited;
+end;
+
+procedure TDAOCConnectionList.DoOnDeleteConnection(AConn: TDAOCConnection);
+begin
+  if Assigned(FOnDeleteConnection) then
+    FOnDeleteConnection(Self, AConn);
+end;
+
+procedure TDAOCConnectionList.DoSetupConnectionEventsAndProperties(
+  AConn: TDAOCConnection);
+begin
+  if Assigned(FOnNewConnection) then
+    FOnNewConnection(Self, AConn);
+end;
+
+function TDAOCConnectionList.FindConnection(ASource: TObject;
+  AConnectionID: Cardinal): TDAOCConnection;
+var
+  iIdx:   integer;
+begin
+  iIdx := IndexOfConnection(ASource, AConnectionID);
+  if iIdx <> -1 then
+    Result := Items[iIdx]
+  else
+    Result := nil;
+end;
+
+function TDAOCConnectionList.GetCount: integer;
+begin
+  Result := FList.Count;
+end;
+
+function TDAOCConnectionList.GetItems(I: integer): TDAOCConnection;
+begin
+  Result := TDAOCConnection(FList[I]);
+end;
+
+function TDAOCConnectionList.IndexOf(AConn: TDAOCConnection): integer;
+begin
+  for Result := 0 to Count - 1 do
+    if Items[Result] = AConn then
+      exit;
+      
+  Result := -1;
+end;
+
+function TDAOCConnectionList.IndexOfConnection(ASource: TObject;
+  AConnectionID: Cardinal): integer;
+begin
+  for Result := 0 to Count - 1 do
+    with Items[Result] do
+      if (Source = ASource) and (ConnectionID = AConnectionID) then
+        exit;
+
+  Result := -1;
+end;
+
+function TDAOCConnectionList.NewDAOCConnection(ASource: TObject;
+  AConnectionID, AServerIP, AClientIP: Cardinal) : TDAOCConnection;
+begin
+  Result := NewDAOCConnectionNeeded;
+  DoSetupConnectionEventsAndProperties(Result);
+  FList.Add(Result);
+
+//  TReintroduceDAOCConnection(Result).LoadRealmRanks(ExtractFilePath(ParamStr(0)) + 'RealmRanks.dat');
+
+  Result.Source := ASource;
+  Result.ConnectionID := AConnectionID;
+  Result.ServerAddr := AServerIP;
+  Result.ClientAddr := AClientIP;
+  Result.PacketHandlerDefFile := ExtractFilePath(ParamStr(0)) + 'packethandlers.ini';
+  Result.InitPacketHandlers;
+
+  TReintroduceDAOCConnection(Result).DoOnConnect;
+end;
+
+function TDAOCConnectionList.NewDAOCConnectionNeeded: TDAOCConnection;
+{ if this function is overridden in a decendant list, make sure you don't call
+  the inherited, it it will leak a connection }
+begin
+  Result := TDAOCConnection.Create;
+end;
+
+procedure TDAOCConnectionList.ProcessDAOCPacket(ASource: TObject;
+  APacket: TGameNetPacket);
+var
+  pConn:    TDAOCConnection;
+begin
+  pConn := FindConnection(ASource, APacket.ConnectionID);
+  if not Assigned(pConn) then
+    pConn := NewDAOCConnection(ASource, APacket.ConnectionID, 0, 0);
+  pConn.ProcessDAOCPacket(APacket);
+end;
+
+end.
