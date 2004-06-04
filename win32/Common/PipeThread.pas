00a5fbdcb225c5acc71bfbcc25e16576963a85eb
teerex <>
2004-06-04 19:49:06 +0000

    changed to not use sync, use WMs to pass data on a list.

--- win32/Common/PipeThread.pas
+++ win32/Common/PipeThread.pas
@@ -13,27 +13,29 @@ unit PipeThread;
 interface
 
 uses
-  Windows, Classes, SysUtils;
+  Windows, Messages, Classes, SysUtils;
 
 type
   TPipeDataNotify = procedure (Sender: TObject; AData: Pointer; ADataLen: integer) of object;
 
   TNamedPipeThread = class(TThread)
   private
+    FMsgList:     TThreadList;
+    FNotifyWnd:   HWND;
     FPipe:        THANDLE;
     FDataBuff:    Pointer;
     FDataBuffSize: integer;
-    FDataBuffUsed: integer;
     FPipeName: string;
     FOnPipeData: TPipeDataNotify;
     FDataBuffLen: integer;
     FPipeOverlap:   TOverlapped;
     FConnected: boolean;
     FOnPipeDisconnected: TNotifyEvent;
-    FOnPipeConnected: TNotifyEvent;
+    FOnPipeConnected:   TNotifyEvent;
     function GetActive: boolean;
     procedure SetPipeName(const Value: string);
     procedure SetDataBuffSize(const Value: integer);
+    procedure ClearMsgList;
   protected
     procedure Execute; override;
     procedure DoOnPipeData;
@@ -41,12 +43,13 @@ type
     procedure DoOnPipeConnected;
     function SetupPipe : boolean;
     procedure CleanupPipe;
+    procedure NotifyWndProc(var Msg: TMessage);
   public
     constructor Create(const APipeName: string);
     destructor Destroy; override;
 
     procedure Shutdown;
-    
+
     property Active: boolean read GetActive;
     property Connected: boolean read FConnected;
     property DataBuffSize: integer read FDataBuffLen write SetDataBuffSize;
@@ -58,6 +61,9 @@ type
 
 implementation
 
+const
+  WM_DATA_AVAIL = WM_USER + 1;
+  
 { TNamedPipeThread }
 
 procedure TNamedPipeThread.CleanupPipe;
@@ -70,19 +76,39 @@ begin
   end;
 end;
 
+procedure TNamedPipeThread.ClearMsgList;
+var
+  I:    integer;
+begin
+  with FMsgList.LockList do
+  try
+    for I := 0 to Count - 1 do
+      Dispose(Items[I]);
+    Clear;
+  finally
+    FMsgList.UnlockList;
+  end;
+end;
+
 constructor TNamedPipeThread.Create(const APipeName: string);
 begin
   inherited Create(true);
+  FMsgList := TThreadList.Create;
   FPipe := INVALID_HANDLE_VALUE;
   FPipeName := APipeName;
-  SetDataBuffSize(16*1024);
+  SetDataBuffSize(4*1024);
+  FNotifyWnd := AllocateHWnd(NotifyWndProc); 
 end;
 
 destructor TNamedPipeThread.Destroy;
 begin
   if FDataBuffSize <> 0 then
     FreeMem(FDataBuff);
-    
+
+  ClearMsgList;
+  FreeAndNil(FMsgList);
+  DeallocateHWnd(FNotifyWnd);
+  
   inherited;
 end;
 
@@ -93,10 +119,34 @@ begin
 end;
 
 procedure TNamedPipeThread.DoOnPipeData;
+var
+  pMsg:     Pointer;
+  dwSize:   Cardinal;
+  pData:    Pointer;
 begin
-  if Assigned(FOnPipeData) then
-    FOnPipeData(Self, FDataBuff, FDataBuffUsed);
-  FDataBuffUsed := 0;
+  repeat
+    with FMsgList.LockList do
+      try
+        if Count > 0 then begin
+          pMsg := First;
+          Delete(0);
+        end
+        else
+          pMsg := nil;
+      finally
+        FMsgList.UnlockList;
+      end;
+
+    if Assigned(pMsg) then begin
+        { first 4 bytes are size }
+      dwSize := PCardinal(pMsg)^;
+        { followed by actual data }
+      pData := Pointer(Cardinal(pMsg) + sizeof(Cardinal));
+      if Assigned(FOnPipeData) then
+        FOnPipeData(Self, pData, dwSize);
+      FreeMem(pMsg);
+    end;  { if msg }
+  until not Assigned(pMsg);
 end;
 
 procedure TNamedPipeThread.DoOnPipeDisconnected;
@@ -107,24 +157,49 @@ end;
 
 procedure TNamedPipeThread.Execute;
 var
-  dwBytes:  Cardinal;
+  dwBytes:      Cardinal;
+  bReadSuccess: boolean;
+  pMsg:         Pointer;
+  bDidRead:     boolean;
 begin
   if not SetupPipe then
     exit;
 
+  bReadSuccess := false;
+
   while not Terminated do begin
     if not FConnected then
       ConnectNamedPipe(FPipe, @FPipeOverlap);
+      { we might want to check the result of the connect operation.  it should
+        return false on success (since we're going overlapped).  GetLastError
+        should return ERROR_IO_PENDING.  If it returns ERROR_PIPE_CONNECTED,
+        we need to manually set our FPipeOverlap.hEvent because otherwise the
+        wait below won't fire.  This probably is a non issue in our case, because
+        the server is always going to come up before a client tries to connect to it }
 
     WaitForSingleObject(FPipeOverlap.hEvent, INFINITE);
 
     if not Terminated then begin
       if FConnected then begin
-        if GetOverlappedResult(FPipe, FPipeOverlap, dwBytes, true) then begin
-          FDataBuffUsed := dwBytes;
-          Synchronize(DoOnPipeData);
-          ReadFile(FPipe, FDataBuff^, FDataBuffSize, dwBytes, @FPipeOverlap);
-        end
+        bDidRead := false;
+        if bReadSuccess or GetOverlappedResult(FPipe, FPipeOverlap, dwBytes, true) then begin
+          repeat
+            if dwBytes > 0 then begin
+              bDidRead := true;
+              GetMem(pMsg, dwBytes + sizeof(Cardinal));
+              PCardinal(pMsg)^ := dwBytes;
+              Move(FDataBuff^, (PChar(pMsg) + 4)^, dwBytes);
+              with FMsgList.LockList do
+                Add(pMsg);
+              FMsgList.UnlockList;
+            end;
+
+            bReadSuccess := ReadFile(FPipe, FDataBuff^, FDataBuffSize, dwBytes, @FPipeOverlap);
+          until not bReadSuccess;
+
+          if bDidRead then
+            PostMessage(FNotifyWnd, WM_DATA_AVAIL, 0, 0);
+        end  { if connected }
         else begin
           FConnected := false;
           DisconnectNamedPipe(FPipe);
@@ -136,7 +211,7 @@ begin
         FConnected := GetOverlappedResult(FPipe, FPipeOverlap, dwBytes, true);
         if FConnected then begin
           Synchronize(DoOnPipeConnected);
-          ReadFile(FPipe, FDataBuff^, FDataBuffSize, dwBytes, @FPipeOverlap);
+          bReadSuccess := ReadFile(FPipe, FDataBuff^, FDataBuffSize, dwBytes, @FPipeOverlap);
         end;
       end;  { if not connected }
     end;
@@ -148,6 +223,12 @@ end;
 function TNamedPipeThread.GetActive: boolean;
 begin
   Result := FPipe <> INVALID_HANDLE_VALUE;
+end;
+
+procedure TNamedPipeThread.NotifyWndProc(var Msg: TMessage);
+begin
+  if Msg.Msg = WM_DATA_AVAIL then
+    DoOnPipeData;
 end;
 
 procedure TNamedPipeThread.SetDataBuffSize(const Value: integer);
